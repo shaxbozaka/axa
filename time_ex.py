868b75f51e98d44b1b56e7d4c97842ff05fb18ca
shaxbozaka <64865484+shaxbozaka@users.noreply.github.com>
2020-12-31 14:03:46 +0500

    Update time_ex.py

--- time_ex.py
+++ time_ex.py
@@ -5,21 +5,23 @@ api_hash = 'a12d841c6ddd543b2010b8fad15ef1d7'
 from pyrogram import Client
 
 
+def time():
+    x = pytz.timezone("Asia/tashkent")
+    time = datetime.now(x)
+    # astimezone method
+    return time
+
+
+def time_has_changed(prev_time):
+    a = datetime.now()
+    if a.second == 55:
+        return True
 
 
 
 with Client("salom pyrogram", api_id, api_hash) as app:
     app.send_message("me", "Greetings from **Pyrogram**!")
-    def time():
-        x = pytz.timezone("Asia/tashkent")
-        time = datetime.now(x)
-        # astimezone method
-        return time
 
-    def time_has_changed(prev_time):
-        a = datetime.now()
-        if a.second == 55:
-            return True
 
     prev_update_time = ""
     while True:
@@ -37,24 +39,25 @@ with Client("salom pyrogram", api_id, api_hash) as app:
                 y = "0" + str(y)
             if y == 60:
                 y = "00"
-            #
-            # client(DeletePhotosRequest(client.get_profile_photos('me')))
-            # file = client.upload_file(f"time/{time().hour}:{time().minute}.jpg")
-            photos = app.get_profile_photos("me")
+  
+
+            if time().weekday() == 4:
+                if time().hour < 13 and time().hour > 10:
+                    
+                    print("passed")
+
+                    file = f"time/{x}:{y}.jpg"
+
+                else:
+                    file = f"time/juma_muborak.jpg"
+
+            else:
+
+                file= f"time/{x}:{y}.jpg"
+                print("passed", time())
             try:
+                photos = app.get_profile_photos("me")
                 app.delete_profile_photos(photos[0].file_id)
             except:
                 print("file not passed")
-            if time().weekday() == 4:
-                if time().hour < 13 and time().hour > 10:
-                    # file = client.upload_file(f"time/juma-{(13 - time().hour) * 60 - time().minute - 1}.jpg")
-                    print("passed")
-                    app.set_profile_photo(photo=f"time/{x}:{y}.jpg")
-                else:
-
-                    # file = client.upload_file(f"time/juma-muborak.jpg")
-                    app.set_profile_photo(photo=f"time/juma_muborak.jpg")
-            else:
-                app.set_profile_photo(photo=f"time/{x}:{y}.jpg")
-                print("passed", time())
-            # client(UploadProfilePhotoRequest(file))
+            app.set_profile_photo(photo=file)
