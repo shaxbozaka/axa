928d8fffefbd5e9146bcef25e6906ed7716ab754
shaxbozaka <64865484+shaxbozaka@users.noreply.github.com>
2020-12-31 14:26:44 +0500

    Update time_ex.py

--- time_ex.py
+++ time_ex.py
@@ -1,9 +1,12 @@
 import pytz
 from datetime import datetime
+from telethon import TelegramClient, sync
+from telethon.tl.functions.photos import UploadProfilePhotoRequest, DeletePhotosRequest
 api_id = '1239727'
 api_hash = 'a12d841c6ddd543b2010b8fad15ef1d7'
 from pyrogram import Client
-
+client = TelegramClient('Sabitov_3', api_id, api_hash)
+client.start()
 
 def time():
     x = pytz.timezone("Asia/tashkent")
@@ -39,25 +42,19 @@ with Client("salom pyrogram", api_id, api_hash) as app:
                 y = "0" + str(y)
             if y == 60:
                 y = "00"
-  
+            #
+            # client(DeletePhotosRequest(client.get_profile_photos('me')))
+            # file = client.upload_file(f"time/{time().hour}:{time().minute}.jpg")
+
 
             if time().weekday() == 4:
                 if time().hour < 13 and time().hour > 10:
-                    
                     print("passed")
-
                     file = f"time/{x}:{y}.jpg"
-
                 else:
                     file = f"time/juma_muborak.jpg"
-
             else:
-
                 file= f"time/{x}:{y}.jpg"
-                print("passed", time())
-            try:
-                photos = app.get_profile_photos("me")
-                app.delete_profile_photos(photos[0].file_id)
-            except:
-                print("file not passed")
+                
+            client(DeletePhotosRequest(client.get_profile_photos('me')))
             app.set_profile_photo(photo=file)
